86e76a04313ee7795b4867cad7322d4ab3cee300
Aleksandr Savinov <Aleksandr.Savinov@primeconcept.co.uk>
2018-03-05 15:19:56 +0300

    Add resource properties

--- src/main/java/com/titaniche/thumbnails/ThumbnailsServlet.java
+++ src/main/java/com/titaniche/thumbnails/ThumbnailsServlet.java
@@ -5,49 +5,78 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Properties;
 import java.util.logging.Logger;
 
-public class ThumbnailsServlet extends HttpServlet {
+public class ThumbnailsServlet extends HttpServlet
+{
 	private final Logger LOGGER = Logger.getLogger(ThumbnailsServlet.class.getSimpleName());
-	//Default path and content type
-	private String filePath = "C:/data/thumbnails";
-	private String contentType = "image/png";
+	private Properties properties = new Properties();
+	private String filePath;
+	private String contentType;
+
+	{
+		try
+		{
+			properties.load(new FileInputStream(new File(getClass().getClassLoader().getResource("config.properties").getFile())));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		filePath = properties.getProperty("filePath");
+		contentType = properties.getProperty("contentType");
+	}
 
 	@Override
-	public void doGet(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws ServletException, IOException {
+	public void doGet(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws ServletException, IOException
+	{
 		String pathInfo = httpRequest.getPathInfo();
 		String thumbUid = null;
-		if (pathInfo != null && pathInfo.startsWith("/")) {
+		if (pathInfo != null && pathInfo.startsWith("/"))
+		{
 			thumbUid = pathInfo.substring(1);
 		}
-		try {
-			if (thumbUid != null) {
+		try
+		{
+			if (thumbUid != null)
+			{
 				File localFile = new File(filePath, thumbUid);
-				if (localFile.exists()) {
+				if (localFile.exists())
+				{
 					LOGGER.info("Cache hit, file exists " + localFile.getAbsolutePath());
 					httpResponse.setContentType(contentType);
 					httpResponse.setContentLength((int) localFile.length());
 					httpResponse.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", thumbUid));
-					try (FileInputStream in = new FileInputStream(localFile)) {
+					try (FileInputStream in = new FileInputStream(localFile))
+					{
 						copy(in, httpResponse.getOutputStream());
 					}
-				} else {
+				}
+				else
+				{
 					LOGGER.info("Local file not found " + localFile.getAbsolutePath());
 					httpResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
 				}
-			} else {
+			}
+			else
+			{
 				LOGGER.info("Can't read uid of thumbnails");
 				httpResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void copy(InputStream in, OutputStream out) throws IOException {
+	private static void copy(InputStream in, OutputStream out) throws IOException
+	{
 		int bytesRead;
 		byte[] buffer = new byte[4096];
-		while ((bytesRead = in.read(buffer)) != -1) {
+		while ((bytesRead = in.read(buffer)) != -1)
+		{
 			out.write(buffer, 0, bytesRead);
 		}
 		out.flush();
